5feb850dd8650061e14ad362036ab65c0ef8fe03
unknown <hrishikeshmandule@gmail.com>
2023-09-15 09:15:52 +0530

    all changes are done in app.tsx and graph.tsx.

--- datafeed/server3.py
+++ datafeed/server3.py
@@ -259,11 +259,15 @@ class App(object):
     def __init__(self):
         self._book_1 = dict()
         self._book_2 = dict()
-        self._data_1 = order_book(read_csv(), self._book_1, 'ABC')
-        self._data_2 = order_book(read_csv(), self._book_2, 'DEF')
-        self._rt_start = datetime.now()
-        self._sim_start, _, _ = next(self._data_1)
-        self.read_10_first_lines()
+        try:
+            self._data_1 = order_book(read_csv(), self._book_1, 'ABC')
+            self._data_2 = order_book(read_csv(), self._book_2, 'DEF')
+            self._rt_start = datetime.now()
+            self._sim_start, _, _ = next(self._data_1)
+            self.read_10_first_lines()
+        except Exception as e:
+            print(f"An error occurred during initialization: {e}")
+            # Handle the error gracefully or perform appropriate actions.
 
     @property
     def _current_book_1(self):
